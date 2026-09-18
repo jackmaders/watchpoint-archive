b1b1254a8990377a6d06d22bee9d9e42af50f3d6
Jack Maders <jackwmaders@gmail.com>
2026-09-18 20:17:59 +0100

    update setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
 			},
 		},
 		environment: "happy-dom",
+		globals: true,
 		include: ["**/*.spec.{ts,tsx}"],
 		setupFiles: ["./vitest/setup.ts"],
 		testTimeout: process.env.CI ? 500 : 1500,
